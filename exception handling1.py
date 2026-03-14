bc69d2c2246ec962d55bae37dce03127cb613cf6
Kaushik ram T <tkaushikram@gmail.com>
2026-03-14 10:45:56 +0530

    Implement basic exception handling in the script

---
+++ exception handling1.py
@@ -0,0 +1,10 @@
+try:
+    a = 10
+    b = 5
+    print(a/b)
+except:
+    print("Something wrong occur in the code")
+else:
+    print("Program succefully executed")
+finally:
+    print("prgm succefully executed")
